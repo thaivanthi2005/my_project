6ba731b7c3e047e623d23998a3c75eef81730bb6
thaivanthi2005 <thaivanthi2005@gmail.com>
2025-08-17 10:02:32 +0700

    cài đặt env

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 app.use(express.static("public"));
+require("dotenv").config();
 
 const router = require("./router/client/index.router");
 
