0524ffdfa7178918c6462eb71889e824400ffebe
thaivanthi2005 <thaivanthi2005@gmail.com>
2025-08-25 23:46:54 +0700

    kết nối database

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
-
 app.set("views", "./views");
 app.set("view engine", "pug");
 app.use(express.static("public"));
 require("dotenv").config();
 
-const router = require("./router/client/index.router");
+const database = require("./config/database.js");
 
-router(app);
+database.connect();
+
+const port = process.env.PORT || 3000;
+
+const router = require("./router/client/index.router");
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
